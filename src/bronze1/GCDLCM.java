424bc7bc861565970484266dc6bb0b4effce1242
SeungIlB <seoung1515@naver.com>
2025-03-20 12:37:07 +0900

    Add: 최대공약수 최소공배수

---
+++ src/bronze1/GCDLCM.java
@@ -0,0 +1,27 @@
+package bronze1;
+
+import java.util.Scanner;
+
+public class GCDLCM {
+    public static int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    public static int lcm(int a, int b) {
+        return (a * b) / gcd(a, b);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+
+        System.out.println(gcd(num1, num2));
+        System.out.println(lcm(num1, num2));
+    }
+}
